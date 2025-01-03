dfc8f939cea94cb8b907a64fdaa0949f465fdddc
Nikita Yeldynov <yeldynov@gmail.com>
2025-01-03 12:43:35 +0200

    * navbar & buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Book, Menu, Sunset, Trees, Zap } from 'lucide-react'
+import { FileQuestion, Menu } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
@@ -28,55 +28,16 @@ import {
 
 const subMenuItemsOne = [
   {
-    title: 'Blog',
-    description: 'The latest industry news, updates, and info',
-    icon: <Book className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Company',
-    description: 'Our mission is to innovate and empower the world',
-    icon: <Trees className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Careers',
-    description: 'Browse job listing and discover our workspace',
-    icon: <Sunset className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Support',
-    description:
-      'Get in touch with our support team or visit our community forums',
-    icon: <Zap className='size-5 shrink-0' />,
-  },
-]
-
-const subMenuItemsTwo = [
-  {
-    title: 'Help Center',
-    description: 'Get all the answers you need right here',
-    icon: <Zap className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Contact Us',
-    description: 'We are here to help you with any questions you have',
-    icon: <Sunset className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Status',
-    description: 'Check the current status of our services and APIs',
-    icon: <Trees className='size-5 shrink-0' />,
-  },
-  {
-    title: 'Terms of Service',
-    description: 'Our terms and conditions for using our services',
-    icon: <Book className='size-5 shrink-0' />,
+    title: 'Why is My Pool Green?',
+    description: '',
+    icon: <FileQuestion className='size-5 shrink-0' />,
   },
 ]
 
 const Navbar = () => {
   return (
     <section className='py-4'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto '>
         <nav className='items-center justify-between hidden lg:flex'>
           <div className='flex items-center gap-6'>
             <div className='flex items-center gap-2'>
@@ -182,26 +143,30 @@ const Navbar = () => {
             </div>
           </div>
           <div className='flex gap-2'>
-            <Button variant='outline'>Contact Us</Button>
-            <Button>Login</Button>
+            <Button variant='outlineblue'>Contact Us</Button>
+            <Button variant='blue'>Login</Button>
           </div>
         </nav>
-        <div className='block lg:hidden'>
+        <div className='block mx-5 lg:hidden'>
           <div className='flex items-center justify-between'>
             <div className='flex items-center gap-2'>
               <img src='logo.png' className='w-full' alt='logo' />
             </div>
             <Sheet>
               <SheetTrigger asChild>
-                <Button variant='outline' size='icon'>
-                  <Menu className='size-4' />
+                <Button
+                  className='p-2 border-none'
+                  variant='outline'
+                  size='icon'
+                >
+                  <Menu className='w-8 h-8' />
                 </Button>
               </SheetTrigger>
               <SheetContent className='overflow-y-auto bg-white'>
                 <SheetHeader>
                   <SheetTitle>
                     <div className='flex items-center gap-2'>
-                      <img src='logo.png' className='w-full' alt='logo' />
+                      <img src='logo.png' className='w-20' alt='logo' />
                     </div>
                   </SheetTitle>
                 </SheetHeader>
@@ -215,7 +180,7 @@ const Navbar = () => {
                   <Accordion type='single' collapsible className='w-full'>
                     <AccordionItem value='products' className='border-b-0'>
                       <AccordionTrigger className='py-0 mb-4 font-semibold hover:no-underline'>
-                        Products
+                        Troubleshooting Guides
                       </AccordionTrigger>
                       <AccordionContent className='mt-2'>
                         {subMenuItemsOne.map((item, idx) => (
@@ -239,112 +204,21 @@ const Navbar = () => {
                         ))}
                       </AccordionContent>
                     </AccordionItem>
-                    <AccordionItem value='resources' className='border-b-0'>
-                      <AccordionTrigger className='py-0 font-semibold hover:no-underline'>
-                        Resources
-                      </AccordionTrigger>
-                      <AccordionContent className='mt-2'>
-                        {subMenuItemsTwo.map((item, idx) => (
-                          <a
-                            key={idx}
-                            className={cn(
-                              'flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground'
-                            )}
-                            href='#'
-                          >
-                            {item.icon}
-                            <div>
-                              <div className='text-sm font-semibold'>
-                                {item.title}
-                              </div>
-                              <p className='text-sm leading-snug text-muted-foreground'>
-                                {item.description}
-                              </p>
-                            </div>
-                          </a>
-                        ))}
-                      </AccordionContent>
-                    </AccordionItem>
                   </Accordion>
                   <a href='#' className='font-semibold'>
-                    Pricing
+                    Our Services
                   </a>
                   <a href='#' className='font-semibold'>
-                    Blog
+                    Work Profile
+                  </a>
+                  <a href='#' className='font-semibold'>
+                    More
                   </a>
                 </div>
                 <div className='pt-4 border-t'>
-                  <div className='grid justify-start grid-cols-2'>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Press
-                    </a>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Contact
-                    </a>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Imprint
-                    </a>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Sitemap
-                    </a>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Legal
-                    </a>
-                    <a
-                      className={cn(
-                        buttonVariants({
-                          variant: 'ghost',
-                        }),
-                        'justify-start text-muted-foreground'
-                      )}
-                      href='#'
-                    >
-                      Cookie Settings
-                    </a>
-                  </div>
                   <div className='flex flex-col gap-3 mt-2'>
-                    <Button variant='outline'>Log in</Button>
-                    <Button>Sign up</Button>
+                    <Button variant='outlineblue'>Log in</Button>
+                    <Button variant='blue'>Sign up</Button>
                   </div>
                 </div>
               </SheetContent>
